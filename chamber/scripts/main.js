fa228d7a05ff4711834d712bf9275adb71c8ca5e
ealmarazr <alm17005@byui.edu>
2023-06-26 09:34:42 -0700

    working with main menu

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,5 +1,5 @@
 var lastChange = document.lastModified;
-document.getElementById("last-updated").innerHTML =  "Last updated: " + lastChange;
+document.getElementById("last-updated").innerHTML = "Last updated: " + lastChange;
 
 /*let outputDate = document.querySelector('date-time')*/
 var today = new Date();
@@ -8,3 +8,19 @@ let year  = today.getFullYear();
 let date  = today.getDate();
 let currentDate = `${month}/${date}/${year}`;
 document.getElementById("date-time").innerHTML = 'Date: ' + currentDate;
+
+/** get the date time as UK format */
+
+const datefieldUK= document.getElementById("today-is");
+const fulldateUK = new Intl.DateTimeFormat("en-UK", { dateStyle:"full"}).format(today);
+/*datefieldUK.innerHTML = `<em>${fulldateUK}</em>`; */
+document.getElementById('today-is').innerHTML = "12345";
+
+/* end of data*/
+
+function toggleMenu() {
+   document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
