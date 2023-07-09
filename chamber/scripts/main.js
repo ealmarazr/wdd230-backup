a47ef8d30da4301d796137ad511149cccbb21443
ealmarazr <alm17005@byui.edu>
2023-07-08 17:38:50 -0700

    re-submitted

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -2,19 +2,23 @@ var lastChange = document.lastModified;
 document.getElementById("last-updated").innerHTML = "Last updated: " + lastChange;
 
 /*let outputDate = document.querySelector('date-time')*/
+
+
 var today = new Date();
-let month = today.getMonth() + 1;
+ let month = today.getMonth() + 1;
 let year  = today.getFullYear();
 let date  = today.getDate();
-let currentDate = `${month}/${date}/${year}`;
-document.getElementById("date-time").innerHTML = 'Date: ' + currentDate;
+
+document.getElementById("today-is").innerHTML = today;
+
+
 
 /** get the date time as UK format */
-
-const datefieldUK= document.getElementById("today-is");
-const fulldateUK = new Intl.DateTimeFormat("en-UK", { dateStyle:"full"}).format(today);
-/*datefieldUK.innerHTML = `<em>${fulldateUK}</em>`; */
-document.getElementById('today-is').innerHTML = "12345";
+/*
+var fulldateUK = new Intl.DateTimeFormat("en-UK", { dateStyle:"full"}).format(today);
+datefieldUK.innerHTML = `<span>${fulldateUK}</span>`; 
+document.getElementById("today-is").innerHTML = 'Date: ' + currentDate;
+*/
 
 /* end of data*/
 
